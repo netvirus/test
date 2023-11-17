79cf45a3fea2a1b678ff4c5794715880c4a16e03
Цупрак Ярослав Игоревич <y.tsuprak@id-mt.ru>
2023-11-17 11:56:09 +0700

    Add getters and setters with toString

--- StartTest.java
+++ StartTest.java
@@ -1,6 +1,16 @@
 public class StartTest {
     public static void main(String[] args) {
-        System.out.println("Start test");
-        int I = 0;
+
+        Computer computer = new Computer(4, 32, "Nvidia", 256);
+
+        Computer computer2 = new Computer();
+        computer2.setCpu(12);
+        computer2.setSsd(512);
+        computer2.setMemory(64);
+        computer2.setVideoCard("Radion");
+
+        System.out.println("Computer1: " + computer);
+
+        System.out.println("Computer2: CPU: " + computer2.getCpu() + " Memory: " + computer2.getMemory() + " Videocard: " + computer2.getVideoCard() + " SSD: " + computer2.getSsd());
     }
 }
